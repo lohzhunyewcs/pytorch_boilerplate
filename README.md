f075533cb3d6960ade82fc573551dfe43325fefc
Loh Zhun Yew <lohzhunyewcs@gmail.com>
2023-03-22 23:17:04 +0800

    Updated current plans for markdown and a simple introduction

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# pytorch_boilerplate
+# PyTorch Boilerplate Code
+
+## Introduction
+This is a repository that covers PyTorch Boilerplate Code covering the full metrics from datasets, models, metrics etc.
+
+Feel free to use the code and let me know aobut any bugs and issues that you find.
+
+## Current Plans
+- [ ] Metrics Support(Recall, Precision etc.)
+- [ ] Computer Vision
+    - [ ] Image Classification
+        - [x] CNN Models
+        - [ ] Transformer Models
+            - [ ] Modify Existing training loop to be more flexible for differeent tasks and models
+            - [ ] Implement Custom Transformer model like ViT
+    - [ ] Semantic Segmentation
+    - [ ] Object Detection
+    - [ ] Instance Segmentation
+- [ ] Natural Language Processing
+    - [ ] Create Base Transformer Models
+        - [ ] Encoder Only
+        - [ ] Decoer Only
+        - [ ] Both in 1
+    - [ ] Text Classification
--- datasets/vision/augmentations.py
+++ datasets/vision/augmentations.py
@@ -3,6 +3,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import torch
 import random
+from torchvision import transforms
 
 # DEFAULT_DATA_TRANSFORMS = {
 #     'train': transforms.Compose([
@@ -113,3 +114,20 @@ def create_transforms(
         ToTensorV2(),
     ]
     return A.Compose(transforms_list)
+
+def use_torch_augmentations(is_train:bool):
+    if is_train:
+        img_transforms = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    else:
+        img_transforms = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    return img_transforms
